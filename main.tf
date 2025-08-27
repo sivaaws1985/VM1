4c3ac462a6daa29e8a97750b7a666cd866506233
sivaaws1985 <sivaaws1985@gmail.com>
2025-08-27 20:31:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "foo" {
 provider "aws" {
     region = "ap-south-1"  
 }
+    }
 
 resource "aws_instance" "foo" {
   ami           = "ami-0f918f7e67a3323f0" #ami-0f918f7e67a3323f0
