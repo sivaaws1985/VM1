f8819f7f3e4064378eec3de2c932f71013ab84f0
sivaaws1985 <sivaaws1985@gmail.com>
2025-08-29 23:32:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,20 @@ resource "aws_instance" "fooec2" {
   ami           = "ami-0f918f7e67a3323f0" #ami-0f918f7e67a3323f0
   instance_type = "t2.micro"
   tags = {
-    Name = "HelloWorldEC21234"
+    Name = "HelloWorldEC212345"
+  }
+}
+resource "aws_instance" "fooec3" {
+  ami           = "ami-0f918f7e67a3323f0" #ami-0f918f7e67a3323f0
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorldEC212346"
+  }
+}
+resource "aws_instance" "fooec4" {
+  ami           = "ami-0f918f7e67a3323f0" #ami-0f918f7e67a3323f0
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorldEC212347"
   }
 }
